7a3baae2f19d71b56a9ba54b7318b86657834433
Ingrzs <ing.ramirozs@gmail.com>
2022-04-29 17:57:12 -0500

    add test unit for method filterbyname

--- test/services/studentservices.test.js
+++ test/services/studentservices.test.js
@@ -1,12 +1,21 @@
 const studentservices = require("./../../lib/services/studentservices")
-const Reader = require("./../../lib/utils/reader")
 
 describe("Tests Para studentservice", () => {
 
-    test("Requerimiento uno obtener student name", () => { 
+    test("Requerimiento uno obtener student id", () => { 
         const student = [{id: "6452a"}];
         const studentbyid = studentservices.filterByid(student, "6452a");
         expect(studentbyid.length).toBe(1);
      
     });
+
+    test("Requerimiento dos obtener student name", () => { 
+        const student = [{name: "warren"}];
+        const studentbyname = studentservices.filterbyname(student, "warren");
+        expect(studentbyname.length).toBe(1);
+     
+    });
+
+
+
 });
