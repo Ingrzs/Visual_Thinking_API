dae255add5d99b07ec3eaaf8c96b0c721ef8a66b
Ingrzs <ing.ramirozs@gmail.com>
2022-04-29 18:17:35 -0500

    add unit test certification true

--- test/services/studentservices.test.js
+++ test/services/studentservices.test.js
@@ -16,6 +16,24 @@ describe("Tests Para studentservice", () => {
      
     });
 
+    test("Requerimiento tres para saber si es certificado es true", () => { 
+        const certificate = [{ id: "6264d5d89f1df827eb84bb23",
+        name: "Warren",
+        email: "Todd@visualpartnership.xyz",
+        credits: 508,
+        enrollments: [
+          "Visual Thinking Intermedio",
+          "Visual Thinking Avanzado"
+        ],
+        previousCourses: 1,
+        haveCertification: true}];
+        const studentbyname = studentservices.filtercertificationtrue(certificate, true);
+        expect(studentbyname[0].name).toBe("warren");
+     
+    });
+
+
+
 
 
 });
