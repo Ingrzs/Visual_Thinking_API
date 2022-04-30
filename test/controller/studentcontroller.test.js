52c62ebfae101bbceaf7936b4396bf1526687b40
Ingrzs <ing.ramirozs@gmail.com>
2022-04-30 17:04:21 -0500

    unit test certification

--- test/controller/studentcontroller.test.js
+++ test/controller/studentcontroller.test.js
@@ -3,8 +3,18 @@ const studentcontroller = require("./../../lib/controllers/studentcontroller")
 describe("Tests Para studentcontroller", () => {
 
     test("Requerimiento uno obtener student id", () => { 
-        
         const studentbyid = studentcontroller.getstudentByid("6264d5d89f1df827eb84bb23");
         expect(studentbyid[0].id).toBe("6264d5d89f1df827eb84bb23");
     });
+
+
+    test("Requerimientos para certificados true",()=>{
+        const studentrue = studentcontroller.getcertificationtrue(true)
+        expect(studentrue[0].email).toBe("Todd@visualpartnership.xyz")
+    })
+
+    test("Requerimientos para certificados false",()=>{
+        const studentfalse = studentcontroller.getcertificationfalse(false)
+        expect(studentfalse[0].email).toBe("Camacho@visualpartnership.xyz")
+    })
 })
